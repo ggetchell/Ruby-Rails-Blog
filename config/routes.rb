292c64c3cbb192c87da62e3084397a56a6ee3718
gregorygetchell <ggetchell@gmail.com>
2020-08-28 14:14:25 -0400

    Added Ruby Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  mount Ckeditor::Engine => '/ckeditor'
+
+  root to: "posts#index"
   devise_for :users
+
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
